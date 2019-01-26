1595b4ea8c4b942887b1d80f1ee3fa8a9d228995
Ivan <ivanfrancechi@hotmail.com>
2019-01-26 20:18:13 -0300

    Email

--- login/settings.py
+++ login/settings.py
@@ -126,5 +126,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'francechiivan@gmail.com'
-EMAIL_HOST_PASSWORD = 'francechi123'
+EMAIL_HOST_USER = 'tu email'
+EMAIL_HOST_PASSWORD = 'tu contraseña'
